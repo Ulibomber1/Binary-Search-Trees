96008acbb626191a37d6b7f1b89e9812dc4254a4
Arius <ulibomber1@gmail.com>
2022-04-06 12:02:11 -0700

    Added clone function and rhs versions of BST constructor. Also added various member functions for the BST class.

--- Binary Search Trees/BST.h
+++ Binary Search Trees/BST.h
@@ -32,12 +32,53 @@ private:
 
 public:
 
-    BinarySearchTree() : root(0) {}
+    BinarySearchTree() : root{ nullptr } {}
+
+    BinarySearchTree(const BinarySearchTree& rhs)
+        : root{ nullptr }
+    {
+        root = clone(rhs.root);
+    }
+
+    BinarySearchTree(BinarySearchTree&& rhs)
+        : root{ rhs.root }
+    {
+        rhs.root = nullptr;
+    }
 
     ~BinarySearchTree()
     {
         makeEmpty();
     }
+
+    BinarySearchTree& operator=(const BinarySearchTree& rhs)
+    {
+        BinarySearchTree copy = rhs;
+        std::swap(*this, copy);
+        return *this;
+    }
+
+    BinarySearchTree& operator=(BinarySearchTree&& rhs)
+    {
+        std::swap(root, rhs.root);
+        return *this;
+    }
+
+    const T& findMin() const
+    {
+        return findMin(root)->element;
+    }
+
+    const T& findMax() const
+    {
+        return findMax(root)->element;
+    }
+
+    bool contains(const T& x) const
+    {
+        return contains(x, root);
+    }
+
     // add memberfct isempty() ...
     bool isEmpty()
     {
@@ -52,6 +93,11 @@ public:
     void insert(const T& x)
     {
         insert(x, root);
+    }
+
+    void remove( const T & x )
+    {
+        remove(x, root);
     }
 
     // KV added; educational and demo purposes
@@ -99,6 +145,62 @@ private:
             ; // duplicate, do nothing
     }
 
+    void remove(const T& x, BinaryNode*& t)
+    {
+        if (t == nullptr)
+            return;
+        if (x < t->element)
+            remove(x, t->left);
+        else if (t->element < x)
+            remove(x, t->right);
+        else if (t->left != nullptr and t->right != nullptr)
+        {
+            t->element = findMin(t->right)->element;
+            remove(t->element, t->right);
+        }
+        else
+        {
+            BinaryNode* oldNode = t;
+
+            if (t->left != nullptr)
+                t = t->left;
+            else
+                t = t->right;
+
+            delete oldNode;
+        }
+    }
+
+    BinaryNode* findMin(BinaryNode* t) const
+    {
+        if (t == nullptr)
+            return nullptr;
+        if (t->left == nullptr)
+            return t;
+
+        return findMin(t->left);
+    }
+
+    BinaryNode* findMax(BinaryNode* t) const
+    {
+        if (t != nullptr)
+            while (t->right != nullptr)
+                t = t->right;
+        return t;
+    }
+
+    bool contains(const T& x, BinaryNode* t) const
+    {
+        if (t == nullptr)
+            return false;
+        else if (x < t->element)
+            return contains(x, t->left);
+        else if (t->element < x)
+            return contains(x, t->right);
+        else
+            return true;  // found a match
+    }
+
     void makeEmpty(BinaryNode*& t)
     {
         if (t != 0)
@@ -133,6 +235,14 @@ private:
         printInternal(t->left, offset + 1);
         printInternal(t->right, offset + 1);
         return;
+    }
+
+    BinaryNode* clone(BinaryNode* t) const
+    {
+        if (t == nullptr)
+            return nullptr;
+        else
+            return new BinaryNode{ t->element, clone(t->left), clone(t->right) };
     }
 };
 
